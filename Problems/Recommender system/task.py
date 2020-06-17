cf40dbad745f70022b9da701053d3bec31bcb13a
Gabriel Porras <ghporras@gmail.com>
2020-06-17 18:47:42 -0500

    feat: Control flow statements exercise

---
+++ Problems/Recommender system/task.py
@@ -0,0 +1,11 @@
+user_age = int(input())
+if user_age <= 16:
+    print("Lion King")
+elif user_age <= 25:
+    print("Trainspotting")
+elif user_age <= 40:
+    print("Matrix")
+elif user_age <= 60:
+    print("Pulp Fiction")
+else:
+    print("Breakfast at Tiffany's")
