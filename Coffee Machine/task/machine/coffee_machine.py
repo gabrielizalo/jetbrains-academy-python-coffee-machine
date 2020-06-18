1878e310cdf67cba38a5537ed28b1affb7f99c3d
Gabriel Porras <ghporras@gmail.com>
2020-06-17 21:10:43 -0500

    feat: added Stage 3/6: Enough coffee for everyone in Coffee Machine

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,10 +1,19 @@
-# Write your code here
+print("Write how many ml of water the coffee machine has:")
+water = int(input())
+print("Write how many ml of milk the coffee machine has:")
+milk = int(input())
+print("Write how many grams of coffee beans the coffee machine has:")
+beans = int(input())
 print("Write how many cups of coffee you will need:")
 no_cups = int(input())
-print("For", no_cups, "cups of coffee you will need:")
-water = no_cups * 200
-milk = no_cups * 50
-beans = no_cups * 15
-print(water, "ml of water")
-print(milk, "ml of milk")
-print(beans, "g of coffee beans")
+
+list_cups = [water // 200, milk // 50, beans // 15]
+min_cups = min(list_cups)
+print(min_cups)
+
+if no_cups == min_cups:
+    print('Yes, I can make that amount of coffee')
+elif no_cups > min_cups:
+    print('No, I can make only', min_cups, 'cups of coffee')
+else:
+    print('Yes, I can make that amount of coffee (and even', min_cups - no_cups, 'more than that)')
