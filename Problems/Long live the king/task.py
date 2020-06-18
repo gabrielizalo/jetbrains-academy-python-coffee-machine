b7c500ac293fbd820891f943850c3f02c7d6060c
Gabriel Porras <ghporras@gmail.com>
2020-06-17 19:12:14 -0500

    feat: Control flow statements exercise

--- Problems/Long live the king/task.py
+++ Problems/Long live the king/task.py
@@ -3,8 +3,9 @@ coordinator_b = int(input())
 
 if 1 < coordinator_a < 8 and 1 < coordinator_b < 8:
     print('8')
-elif (coordinator_a == 1 and coordinator_b == 1) or (coordinator_a == 8 and coordinator_b == 8) or (
-        coordinator_a == 1 and coordinator_b == 8) or (coordinator_a == 8 and coordinator_b == 1):
+elif (coordinator_a == 1 and coordinator_b == 1) or (coordinator_a == 8 and coordinator_b == 8):
+    print('3')
+elif (coordinator_a == 1 and coordinator_b == 8) or (coordinator_a == 8 and coordinator_b == 1):
     print('3')
 else:
     print('5')
