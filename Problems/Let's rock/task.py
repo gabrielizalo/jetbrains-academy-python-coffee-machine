02eb368920086d6704f52ee64f0e26796a55c17b
Gabriel Porras <ghporras@gmail.com>
2020-07-02 18:25:45 -0500

    feat: Object-oriented programming exercise

---
+++ Problems/Let's rock/task.py
@@ -0,0 +1,11 @@
+# start a RockBand here
+class RockBand:
+    genre = "rock"
+    n_members = 4
+    key_instruments = ["electric guitar", "drums"]
+
+
+slayer = RockBand()
+print(slayer.genre)
+print(slayer.n_members)
+print(slayer.key_instruments)
